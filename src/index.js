5c8e2b2de2062812babcd4f4887a5f83fef4ba91
alienao <armin.orujyan@gmail.com>
2022-05-08 23:26:23 +0400

    The UseState Hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FaStar } from 'react-icons/fa';
+
+// function App(){
+//   const [status, setStatus]= useState("Not Delivered");
+
+//   return(
+//     <div>
+//       <h1>The package is: {status}</h1>
+//       <button onClick = {() => setStatus("Delivered")}>Deliver</button>
+//     </div>
+//   );
+// }
+
+// function App(){
+//   const [checked, setChecked] = useState(false);
+
+//   return(
+//     <div>
+//       <input type = "checkbox" value={checked} onChange = {() => setChecked(!checked)}/>
+//       <p>{checked ? "checked" : "not checked"}</p>
+//     </div>
+//   );
+// }
+
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? 'red' : 'gray'} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
+}
+
+function App() {
+  return <StarRating totalStars={10} />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +58,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
